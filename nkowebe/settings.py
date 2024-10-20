2b1f5a123123b5a29712d98b7fce05fae7325760
WizCode <adewale.oladiti28@gmail.com>
2024-10-20 18:32:26 +0100

    added vercel

--- nkowebe/settings.py
+++ nkowebe/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-mhwo6vx)waf#lgdo9-^(8)hpg&@4+tl2t^#vz+@i!-pdm8sw1r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
