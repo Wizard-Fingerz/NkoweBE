db76fb2328a72fa6d91031745c9b1451db1200b7
WizCode <adewale.oladiti28@gmail.com>
2024-10-20 18:54:28 +0100

    updated static

--- nkowebe/settings.py
+++ nkowebe/settings.py
@@ -149,7 +149,10 @@ AUTH_USER_MODEL = 'account.CustomUser'
 
 STATIC_URL = 'static/'
 
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For production
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
