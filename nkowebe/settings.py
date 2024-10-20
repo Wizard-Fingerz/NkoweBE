c3bd17d50adca2ac56ae55c670fd3b1990afbf3c
WizCode <adewale.oladiti28@gmail.com>
2024-10-20 19:23:03 +0100

    updated

--- nkowebe/settings.py
+++ nkowebe/settings.py
@@ -147,7 +147,7 @@ AUTH_USER_MODEL = 'account.CustomUser'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
