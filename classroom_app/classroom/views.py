a63cf62ccfb124ead2f7111c9bc13f6a5771e004
WizCode <adewale.oladiti28@gmail.com>
2025-04-20 15:25:12 +0100

    updated pagination

--- classroom_app/classroom/views.py
+++ classroom_app/classroom/views.py
@@ -9,9 +9,9 @@ from rest_framework import viewsets, pagination
 
 
 class CustomPagination(pagination.PageNumberPagination):
-    page_size = 15
+    page_size = 100
     page_size_query_param = 'page_size'
-    max_page_size = 15
+    max_page_size = 100
 
     def get_paginated_response(self, data):
         return Response({
