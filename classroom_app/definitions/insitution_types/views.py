923e2414ab9090f054c12fab48c2b68801688cd4
WizCode <adewale.oladiti28@gmail.com>
2025-04-20 14:53:02 +0100

    updated

--- classroom_app/definitions/insitution_types/views.py
+++ classroom_app/definitions/insitution_types/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+
+from classroom_app.classroom.views import CustomPagination
 from .models import InstitutionType
 from .serializers import InstitutionTypeSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -7,6 +9,7 @@ class InstitutionTypeViewSet(viewsets.ModelViewSet):
     queryset = InstitutionType.objects.filter(is_deleted=False)  # Exclude deleted items
     serializer_class = InstitutionTypeSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    pagination_class = CustomPagination
 
     def perform_destroy(self, instance):
         # Soft delete by setting `is_deleted` to True
