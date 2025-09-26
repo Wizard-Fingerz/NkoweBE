67a908cf74fe9e81fe69815539ef9b14c9bd9aad
WizCode <adewale.oladiti28@gmail.com>
2025-09-26 03:35:17 +0100

    Update CORS settings to allow additional frontend origins
    - Added "http://localhost:5174" and "http://127.0.0.1:5174" to CORS_ALLOWED_ORIGINS in settings.py to support new frontend instances.

--- nkowebe/settings.py
+++ nkowebe/settings.py
@@ -154,7 +154,9 @@ USE_TZ = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Your frontend origin
+    "http://localhost:5174",  # Your frontend origin
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
     # Add any other allowed origins
 ]
 
