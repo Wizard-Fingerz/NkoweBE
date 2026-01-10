a0be8b74b2c53412f86a76f6e51489d953557694
Adewale <adewale.oladiti28@gmail.com>
2026-01-10 11:53:38 +0100

    Register models in admin interface for library app
    - Added admin configurations for Author, Publisher, Book, and Member models to enhance the admin interface.
    - Implemented list display, search fields, and filters for improved data management and accessibility.

--- library_app/admin.py
+++ library_app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Author, Publisher, Book, Member
 
-# Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'dob')
+    search_fields = ('first_name', 'last_name')
+    list_filter = ('dob',)
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'website')
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publisher', 'publication_date', 'isbn', 'total_copies', 'available_copies', 'institution')
+    search_fields = ('title', 'isbn')
+    list_filter = ('publisher', 'publication_date', 'institution')
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone_number', 'joined_date', 'is_active', 'institution')
+    search_fields = ('user__username', 'user__first_name', 'user__last_name', 'phone_number')
+    list_filter = ('joined_date', 'is_active', 'institution')
